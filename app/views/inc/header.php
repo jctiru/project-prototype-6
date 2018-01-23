9ccba4fe6da9186fb579cbd81503448716927748
jctiru <jonathanchristiantiru@gmail.com>
2018-01-23 18:57:07 +0800

    Initial commit

---
+++ app/views/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="id=edge">
+	<title><?php echo SITENAME; ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/style.css">
+
+</head>
+<body>
